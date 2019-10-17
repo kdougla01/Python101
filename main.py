de89be5bb6882e1514314e502fd83f3545276daa
Mr Douglass <k.dougla01@pudseygrangefield.co.uk>
2019-10-17 11:30:55 +0100

    v0.3
    dev version of main.py

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ name = input("What is your name? ")
 print (f"Well hello, {name}")
 
 age = int(input("What is your age? "))
-
-if age >= 18:
-	print ("You must be an adult then!")
-elif age >= 13:
-	print ("You must be a teenager")
-else:
-	print ("You are a child")
+ print ("Dev version")
